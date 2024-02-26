3e95399c3a0e4bab116eed8b262d57e51be22502
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-02-26 17:04:38 +0100

    create agency queries

---
+++ src/components/forms/agency-details.tsx
@@ -0,0 +1,14 @@
+import { Agency } from "@prisma/client"
+
+type Props = {
+    data?: Partial<Agency>
+}
+
+const AgencyDetails = (props: Props) => {
+    
+  return (
+    <div>AgencyDetails</div>
+  )
+}
+
+export default AgencyDetails
