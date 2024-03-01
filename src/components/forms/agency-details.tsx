99e704b5db01fe0a767f257cb7e512550c6e3501
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-03-01 11:33:17 +0100

    create loading, delet and handlesubmmit function

--- src/components/forms/agency-details.tsx
+++ src/components/forms/agency-details.tsx
@@ -6,6 +6,7 @@ import { Agency } from "@prisma/client";
 import * as z from "zod";
 import { zodResolver } from '@hookform/resolvers/zod'
 import { NumberInput } from '@tremor/react'
+import { v4 } from 'uuid'
 
 import { useToast } from "@/components/ui/use-toast";
 import { AlertDialog } from "@radix-ui/react-alert-dialog";
@@ -28,7 +29,10 @@ import {
 import FileUpload from "../global/file-upload";
 import { Input } from "../ui/input";
 import { Switch } from "../ui/switch";
-import { saveActivityLogsNotification, updateAgencyDetails } from "@/lib/queries";
+import { deleteAgency, initUser, saveActivityLogsNotification, updateAgencyDetails, upsertAgency } from "@/lib/queries";
+import { Button } from "../ui/button";
+import Loading from "../global/loading";
+import { AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "../ui/alert-dialog";
 
 type Props = {
   data?: Partial<Agency>;
@@ -75,8 +79,93 @@ const AgencyDetails = ({ data }: Props) => {
     }
   }, [data])
 
+  const handleSubmit = async (values: z.infer<typeof FormSchema>) => {
+    try {
+      let newUserData
+      let custId
+      if (!data?.id) {
+        const bodyData = {
+          email: values.companyEmail,
+          name: values.name,
+          shipping: {
+            address: {
+              city: values.city,
+              country: values.country,
+              line1: values.address,
+              postal_code: values.zipCode,
+              state: values.zipCode,
+            },
+            name: values.name,
+          },
+          address: {
+            city: values.city,
+            country: values.country,
+            line1: values.address,
+            postal_code: values.zipCode,
+            state: values.zipCode,
+          },
+        }
+      }
 
-  const handleSubmit = async (values: z.infer<typeof FormSchema>) => {}
+      newUserData = await initUser({ role: 'AGENCY_OWNER' })
+      if (!data?.customerId && !custId) return
+
+      const response = await upsertAgency({
+        id: data?.id ? data.id : v4(),
+        customerId: data?.customerId || custId || '',
+        address: values.address,
+        agencyLogo: values.agencyLogo,
+        city: values.city,
+        companyPhone: values.companyPhone,
+        country: values.country,
+        name: values.name,
+        state: values.state,
+        whiteLabel: values.whiteLabel,
+        zipCode: values.zipCode,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        companyEmail: values.companyEmail,
+        connectAccountId: '',
+        goal: 5,
+      })
+      toast({
+        title: 'Created Agency',
+      })
+      if (data?.id) return router.refresh()
+      if (response) {
+        return router.refresh()
+      }
+    } catch (error) {
+      console.log(error)
+      toast({
+        variant: 'destructive',
+        title: 'Oppse!',
+        description: 'could not create your agency',
+      })
+    }
+  }
+  const handleDeleteAgency = async () => {
+    if (!data?.id) return
+    setDeletingAgency(true)
+    //WIP: discontinue the subscription
+    try {
+      const response = await deleteAgency(data.id)
+      toast({
+        title: 'Deleted Agency',
+        description: 'Deleted your agency and all subaccounts',
+      })
+      router.refresh()
+    } catch (error) {
+      console.log(error)
+      toast({
+        variant: 'destructive',
+        title: 'Oppse!',
+        description: 'could not delete your agency ',
+      })
+    }
+    setDeletingAgency(false)
+  }
+  
   return (
     <AlertDialog>
       <Card className="w-full">
@@ -304,8 +393,56 @@ const AgencyDetails = ({ data }: Props) => {
                   />
                 </div>
               )}
+              <Button
+                type="submit"
+                disabled={isLoading}
+              >
+                {isLoading ? <Loading /> : 'Save Agency Information'}
+              </Button>
         </form>
         </Form> 
+
+        {data?.id && (
+            <div className="flex flex-row items-center justify-between rounded-lg border border-destructive gap-4 p-4 mt-4">
+              <div>
+                <div>Danger Zone</div>
+              </div>
+              <div className="text-muted-foreground">
+                Deleting your agency cannpt be undone. This will also delete all
+                sub accounts and all data related to your sub accounts. Sub
+                accounts will no longer have access to funnels, contacts etc.
+              </div>
+              <AlertDialogTrigger
+                disabled={isLoading || deletingAgency}
+                className="text-red-600 p-2 text-center mt-2 rounded-md hove:bg-red-600 hover:text-white whitespace-nowrap"
+              >
+                {deletingAgency ? 'Deleting...' : 'Delete Agency'}
+              </AlertDialogTrigger>
+            </div>
+          )}
+          
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle className="text-left">
+                Are you absolutely sure?
+              </AlertDialogTitle>
+              <AlertDialogDescription className="text-left">
+                This action cannot be undone. This will permanently delete the
+                Agency account and all related sub accounts.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter className="flex items-center">
+              <AlertDialogCancel className="mb-2">Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                disabled={deletingAgency}
+                className="bg-destructive hover:bg-destructive"
+                onClick={handleDeleteAgency}
+              >
+                Delete
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+          
         </CardContent>
       </Card>
     </AlertDialog>
--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -193,3 +193,90 @@ export const getAuthUserDetails = async () => {
     })
     return response
   }
+
+  export const deleteAgency = async (agencyId: string) => {
+    const response = await db.agency.delete({ where: { id: agencyId } })
+    return response
+  }
+
+  export const initUser = async (newUser: Partial<User>) => {
+    const user = await currentUser()
+    if (!user) return
+  
+    const userData = await db.user.upsert({
+      where: {
+        email: user.emailAddresses[0].emailAddress,
+      },
+      update: newUser,
+      create: {
+        id: user.id,
+        avatarUrl: user.imageUrl,
+        email: user.emailAddresses[0].emailAddress,
+        name: `${user.firstName} ${user.lastName}`,
+        role: newUser.role || 'SUBACCOUNT_USER',
+      },
+    })
+  
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        role: newUser.role || 'SUBACCOUNT_USER',
+      },
+    })
+  
+    return userData
+  }
+
+  export const upsertAgency = async (agency: Agency, price?: Plan) => {
+    if (!agency.companyEmail) return null
+    try {
+      const agencyDetails = await db.agency.upsert({
+        where: {
+          id: agency.id,
+        },
+        update: agency,
+        create: {
+          users: {
+            connect: { email: agency.companyEmail },
+          },
+          ...agency,
+          SidebarOption: {
+            create: [
+              {
+                name: 'Dashboard',
+                icon: 'category',
+                link: `/agency/${agency.id}`,
+              },
+              {
+                name: 'Launchpad',
+                icon: 'clipboardIcon',
+                link: `/agency/${agency.id}/launchpad`,
+              },
+              {
+                name: 'Billing',
+                icon: 'payment',
+                link: `/agency/${agency.id}/billing`,
+              },
+              {
+                name: 'Settings',
+                icon: 'settings',
+                link: `/agency/${agency.id}/settings`,
+              },
+              {
+                name: 'Sub Accounts',
+                icon: 'person',
+                link: `/agency/${agency.id}/all-subaccounts`,
+              },
+              {
+                name: 'Team',
+                icon: 'shield',
+                link: `/agency/${agency.id}/team`,
+              },
+            ],
+          },
+        },
+      })
+      return agencyDetails
+    } catch (error) {
+      console.log(error)
+    }
+  }
